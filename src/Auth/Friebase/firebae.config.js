898d7ecb2ce90a4ee7a8bea40c60738ccfeecfdb
Sadek Ali <sadekali1530@gmail.com>
2023-11-25 12:31:53 +0600

    set firebase.cofing in env file

--- src/Auth/Friebase/firebae.config.js
+++ src/Auth/Friebase/firebae.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-SS_3xIqu3Y5BAoi2xPoJayD0Jl5EvNI",
-  authDomain: "asset-d91a1.firebaseapp.com",
-  projectId: "asset-d91a1",
-  storageBucket: "asset-d91a1.appspot.com",
-  messagingSenderId: "263583408957",
-  appId: "1:263583408957:web:2237db5b577ebdc6065b54"
+  apiKey:import.meta.envVITE_apiKey ,
+  authDomain:import.meta.envVITE_authDomain ,
+  projectId:import.meta.envVITE_projectId ,
+  storageBucket:import.meta.envVITE_storageBucket ,
+  messagingSenderId:import.meta.envVITE_messagingSenderId ,
+  appId:import.meta.envVITE_appId ,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+ export const app = initializeApp(firebaseConfig);
